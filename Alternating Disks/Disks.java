478bdb2e3b78e5cc16f720a38993ba35cc1fd663
Gabe Salac <gsalac7@gmail.com>
2017-02-21 16:32:36 -0800

    Add files via upload

--- Alternating Disks/Disks.java
+++ Alternating Disks/Disks.java
@@ -1,7 +1,11 @@
+/*Angelo Salac
+ * 2/24/17
+ * Programming Assignment 1
+ * CPSC 335
+ */
+
 import java.util.Scanner;
 
-//1 = dark
-//0 = light
 public class Disks {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
@@ -12,74 +16,94 @@ public class Disks {
 		numDisks *=2;
 		
 		//disks are implemented as an array of 1's and 0's
+		//1 = dark
+		//0 = light
 		int [] disks = new int[numDisks];
-		for (int i = 0; i < numDisks; i++) {
-			if (i%2 == 0) {
-				disks[i] = 1;
-			}
-			else {
-				disks[i] = 0;
-			}
-		}
+		initializeDisks(disks);
 		
+		
+		System.out.println("This is the initial placement of the disks: ");
 		displayDisks(disks); //display initial disks
-		//greedy(disks); //perform the first greedy algorithm.
+		System.out.println("Now implementing left-to-right algorithm");
+		greedy(disks); //perform the first greedy algorithm.
+		
+		initializeDisks(disks);
+		System.out.println("This is the initial placement of the disks: ");
+		displayDisks(disks); //display initial disks
+		System.out.println("Now implementing lawnmower algorithm");
 		lawnmower(disks); //perform the first lawnmower algorithm.
 		
 		read.close();
 	}
 	
-	
+	//assign the array values
+	public static void initializeDisks(int [] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (i%2 == 0) {
+				arr[i] = 1;
+			}
+			else {
+				arr[i] = 0;
+			}
+		}
+	}
 	public static void greedy(int []arr) {
 		boolean compare = true;
 		int swapCounter = 0;
 		
-		while(compare) {
-			compare = false;
-			for (int i = 0; i < arr.length - 1; i++) {
-				if (arr[i] > arr[i+1]) {
-					swap(i, i+1, arr);
-					swapCounter++;
-					compare = true;
+		while(compare) { //loop while the disks arent sorted
+			compare = false; //until theres not a comparison, list is not sorted
+			for (int i = 0; i < arr.length - 1; i++) { //go through loop from left to right, then restart
+				if (arr[i] > arr[i+1]) {	//if the next disk is light, then swap with the dark disk.
+					swap(i, i+1, arr); //swap
+					swapCounter++; //count the swap
+					compare = true; //set the comparison to true so we know there was a comparison made this iteration
 				}
 			}
 			displayDisks(arr);
-		}
+		} //loop starts over is compare = true
 		System.out.println("Number of swaps: " + swapCounter);
 	}
 	
 	public static void lawnmower(int []arr) {
 		int swapCounter = 0;
 		boolean compare = true;
-		while(compare) {
-			int i = 0, j = arr.length - 1;
-			compare = false;
-			while(i < arr.length - 1) {
-				if (arr[i] > arr[i+1]) {
-					swap(i, i+1, arr);
-					compare = true;
-					swapCounter++;
+		
+		while(compare) { //loop while array is not sorted
+			int i = 0, j = arr.length - 1; //assign values for i and j
+			compare = false; 
+			//first pass from the left
+			while(i < arr.length - 1) { //from left to right
+				if (arr[i] > arr[i+1]) { //if the next disk is light, then swap
+					swap(i, i+1, arr); //swap
+					compare = true; //let us know a swap took place.
+					swapCounter++; //add to the swap counter
 				}
 				i++;
-			}
-			while(j > 0) {
-				if (arr[j] < arr[j-1]) {
-					swap(j, j - 1, arr);
-					compare = true;
-					swapCounter++;
+			}//now we are on the right side
+			displayDisks(arr);
+			//then pass from the right
+			while(j > 0) { //now we go from right to left
+				if (arr[j] < arr[j-1]) { //if the disk before is dark, then swap
+					swap(j, j - 1, arr); //swap
+					compare = true; //let us know a swap took place
+					swapCounter++; //add to the swap counter
 				}
 				j--;
-			}
-			displayDisks(arr);
-		}
+			}//now we are back on the left
+			displayDisks(arr); //display disks after the two passes
+		} //loop until the array is sorted
+		System.out.println("Number of swaps: " + swapCounter);
 	}
 	
+	//swap function
 	public static void swap(int i, int j, int []arr) {
 		int c = arr[j];
 		arr[j] = arr[i];
 		arr[i] = c;
 	}
 	
+	//function to display the disks
 	public static void displayDisks(int [] arr) {
 		for(int i = 0; i < arr.length; i++) {
 			if (arr[i] == 1)
