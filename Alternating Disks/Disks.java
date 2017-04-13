c151483c5a2d83ca912dca138f219907d2b3cea3
Gabe Salac <gsalac7@gmail.com>
2017-04-12 21:20:26 -0700

    Update Disks.java

--- Alternating Disks/Disks.java
+++ Alternating Disks/Disks.java
@@ -48,51 +48,36 @@ public class Disks {
 		}
 	}
 	public static void greedy(int []arr) {
-		boolean compare = true;
 		int swapCounter = 0;
-		
-		while(compare) { //loop while the disks arent sorted
-			compare = false; //until theres not a comparison, list is not sorted
+		for (int x = 1; x <= arr.length; x++) { //loop while the disks aren't sorted
 			for (int i = 0; i < arr.length - 1; i++) { //go through loop from left to right, then restart
 				if (arr[i] > arr[i+1]) {	//if the next disk is light, then swap with the dark disk.
 					swap(i, i+1, arr); //swap
 					swapCounter++; //count the swap
-					compare = true; //set the comparison to true so we know there was a comparison made this iteration
 				}
 			}
-			displayDisks(arr);
-		} //loop starts over is compare = true
+		} //go back to the left and loop again
+		displayDisks(arr);
 		System.out.println("Number of swaps: " + swapCounter);
 	}
 	
 	public static void lawnmower(int []arr) {
 		int swapCounter = 0;
-		boolean compare = true;
-		
-		while(compare) { //loop while array is not sorted
-			int i = 0, j = arr.length - 1; //assign values for i and j
-			compare = false; 
-			//first pass from the left
-			while(i < arr.length - 1) { //from left to right
-				if (arr[i] > arr[i+1]) { //if the next disk is light, then swap
-					swap(i, i+1, arr); //swap
-					compare = true; //let us know a swap took place.
-					swapCounter++; //add to the swap counter
+		for (int i = 0; i < arr.length; i++) { //repeat until all disks are swapped
+			for (int k = 0; k < arr.length - 1; k++) { //go from left to right
+				if (arr[k] > arr[k + 1]) {
+					swap(k, k + 1, arr);
+					swapCounter++;
 				}
-				i++;
-			}//now we are on the right side
-			displayDisks(arr);
-			//then pass from the right
-			while(j > 0) { //now we go from right to left
-				if (arr[j] < arr[j-1]) { //if the disk before is dark, then swap
-					swap(j, j - 1, arr); //swap
-					compare = true; //let us know a swap took place
-					swapCounter++; //add to the swap counter
+			}
+			for (int j = arr.length - 1; j > 0; j--) { //go from right to left
+				if (arr[j] < arr[j - 1]) {
+					swap(j, j - 1, arr);
+					swapCounter++;
 				}
-				j--;
-			}//now we are back on the left
-			displayDisks(arr); //display disks after the two passes
-		} //loop until the array is sorted
+			}
+		}
+		displayDisks(arr);
 		System.out.println("Number of swaps: " + swapCounter);
 	}
 	
