95105a00041b9926484b6c48b064724e7bc3d5ad
Gabe Salac <gsalac7@gmail.com>
2017-04-15 20:30:56 -0700

    Update Rectilinear.java

--- TSP/Rectilinear.java
+++ TSP/Rectilinear.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Rectilinear {
+	static int bestDist;
 	public static void main(String [] args) {
 		Scanner read = new Scanner(System.in);
 		
 		System.out.println("Enter the number of vertices (>2): ");
 		int vertices = read.nextInt();
-		
+		int dist;
 		point[] points = new point[vertices]; 
 		point[] bestA = new point[vertices];
 		System.out.println("Enter the points; make sure they are distinct: ");
@@ -19,26 +20,27 @@ public class Rectilinear {
 			points[i] = new point(x, y);
 		}
 		
-		//Assign first permutation's weight as the best weight
+		
 		for (int i = 0; i < points.length; i++) {
     		points[i].print();
     	}
     	System.out.println();
-		int weight = 0;
-		for (int i = 0; i < points.length - 1; i++) {
-			int d = Math.abs(points[i].x - points[i+1].x) + Math.abs(points[i].y - points[i+1].y);
-			weight += d;
-		}
-		int best = weight;
+    	double startTime = System.nanoTime();
+		dist = farthest(points);
+		bestDist = vertices*dist;
 		
+    	
 		for (int i = 0 ; i < points.length; i++) {
 			bestA[i] = points[i];
 		}
 		System.out.println();
 		
-		permutation(points, vertices, best, bestA);
+		permutation(points, vertices, bestA);
 		printCycle(bestA);
-		System.out.println("The best distance: " + best);
+		System.out.println("The best distance: " + bestDist);
+		double endTime   = System.nanoTime();
+		double totalTime = (endTime - startTime)/1000000/1000; //convert time to seconds
+		System.out.println("Elapsed Time: " + totalTime + " seconds");
 		read.close();
 	}
 	
@@ -50,25 +52,38 @@ public class Rectilinear {
 		System.out.println();
 	}
 	
-	public static void permutation(point[] v, int n, int best, point[] bestA) {		
+	public static int farthest(point [] v) {
+		int max = 0, dist;
+		for (int i = 0; i < v.length - 1; i++) {
+			for (int j = 0; j < v.length; j++) {
+				dist = Math.abs(v[i].x - v[j].x) + Math.abs(v[i].y - v[j].y);
+				if (max < dist) {
+					max = dist;
+				}
+			}
+		}
+		return max;
+	}
+	
+	public static void permutation(point[] v, int n, point[] bestA) {		
         if (n == 1) {
         	int dist = 0;
         	for (int i = 0; i < v.length - 1; i++) {
-        		
         		int d = Math.abs(v[i].x - v[i+1].x) + Math.abs(v[i].y - v[i+1].y);
         		dist += d;
         	}
-        	 //find the least distance
-        	if (dist < best) {
-        		best = dist;
+        	dist += Math.abs(v[v.length - 1].x - v[0].x) + Math.abs(v[v.length - 1].y - v[0].y);
+        	//find the least distance
+        	if (dist < bestDist) {
+        		bestDist = dist;
         		for (int k = 0; k < v.length; k++) {
-                	bestA[k] = v[k];
+                	bestA[k] = v[k]; //find the best array
                 }
         	}        	
         } 
         else {
         	for (int i = 0; i < n - 1; i++) {
-        		permutation(v, n - 1, best, bestA);
+        		permutation(v, n - 1, bestA);
         		if (n%2 == 0) {
         			point temp = v[i];
         			v[i] = v[n - 1];
@@ -80,7 +95,7 @@ public class Rectilinear {
         			v[n - 1] = temp;
         		}
         	}
-        	permutation(v, n - 1, best, bestA);
+        	permutation(v, n - 1, bestA);
         }
     }
 }
