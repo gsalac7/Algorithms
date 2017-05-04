74a3ee4c82e9a32a0cb42e65dd51a654eeaaa9a4
Gabe Salac <gsalac7@gmail.com>
2017-05-04 16:41:11 -0700

    Update Hashing.java

--- Hashing/Hashing.java
+++ Hashing/Hashing.java
@@ -18,7 +18,7 @@ public class Hashing {
 		String fileName = read.nextLine();
 		
 		try {
-			hashFile("C:\\Users\\gsalac7\\Desktop\\" + fileName + ".txt", T1, T2, tableSize);
+			hashFile("/Users/gsalac7/Desktop/" + fileName + ".txt", T1, T2, tableSize);
 		} catch (IOException ex) {
 			System.out.println("Error reading the file");
 		}
@@ -35,8 +35,8 @@ public class Hashing {
 		BufferedReader in = new BufferedReader(new FileReader(fname));
 		try {	
 			String line;
-			while((line = in.readLine())!=null) {
-				hash(T1, T2, hashFunction1(line, tableSize), hashFunction2(line, tableSize), line);
+			while((line = in.readLine()) != null) {
+				hash(T1, T2, hashFunction1(line), line);
 			}
 		} catch(Exception Ex){
 			System.out.println("File not found");
@@ -45,38 +45,44 @@ public class Hashing {
 		in.close();
 	} 
 	
-	public static void hash(String [] T1, String [] T2, int key1, int key2, String line) {
+	public static void hash(String [] T1, String [] T2, int key1, String line) {
 		if (T1[key1] == null) {
 			T1[key1] = line;
 			System.out.println("String <" + line + "> will be placed at Table 1[" + key1 + "]");
 		} else {
-			T2[key2] = line;
-			System.out.println("String <" + line + "> will be placed at Table 2[" + key2 + "]");
+			String temp = T1[key1];
+			int newKey2 = hashFunction2(temp);
+			T2[newKey2] = temp;
+			T1[key1] = line;
+			System.out.println("String <" + line + "> will be placed at Table 1[" + key1 + "] replacing " + temp);
+			System.out.println("String <" + temp + "> will be placed at Table 2[" + newKey2 + "]");
 		}
 	}
 	
-	public static int hashFunction1(String line, int tableSize) {
+	public static int hashFunction1(String line) {
 		int value = 0;
 		
 		for (int i = 0; i < line.length(); i++) {
-			value = line.charAt(i) * (int)Math.pow(31, i);
+			value += (int)line.charAt(i) * (int)Math.pow(31, i);
 			
 		}
-		value = value % tableSize;
+		value = value % 17;
 		if (value < 0) {
-			value += tableSize;
+			value += 17;
 		}
 		return value;
 	}
 	
-	public static int hashFunction2(String line, int tableSize) {
+	public static int hashFunction2(String line) {
 		int value = 0;
 		
 		for (int i = 0; i < line.length(); i++) {
-			value = line.charAt(line.length() - i - 1) * (int)Math.pow(31, i);
+			value += (int)line.charAt(line.length() - 1 - i) * (int)Math.pow(31, i);
 		}
-		value = value % tableSize;
-		
+		value = value % 17;
+		if (value < 0) {
+			value += 17;
+		}
 		return value;
 	}
 	
